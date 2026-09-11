3da5fa7e7f25a264937d1b76af3aec9abd2b3a86
Ce Cheng <2601889@singaporetech.edu.sg>
2026-09-11 16:45:11 +0800

    Updated Requirements

--- auditor.py
+++ auditor.py
@@ -1,19 +1,49 @@
 #create variable stock quantity and set to zero
 stock_quantity = 0
-
+error_count = 0
 #Requesting user to input stock quantity until user types quit
+
 while True:
     user_input = input("Enter stock quantity.\n")
 
-    if user_input == "quit":  
-        break
+    if user_input.lower() == "quit":
+     print("The total Units Processed is {}and number of Failed/Rejected Entries is:{} \n".format(stock_quantity, error_count) )
+     break
 
-    #Handle invalid input
+
     if user_input.isdigit():
         stock_quantity += int(user_input)
         print("Current inventory count is:",stock_quantity)
-    else:
-        print("Error! Please input whole numbers!\n")
 
+        if int(user_input) > 500: 
+               print("Alert! Total inventory has exceeded 500units!")
+               break 
+        
+    else: 
+        print("Error! Please input positive whole numbers only!\n")
+        error_count += 1
+
+
+    
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+    
+            
 
 
